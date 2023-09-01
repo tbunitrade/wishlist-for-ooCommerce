865039986301726e1e4913dd563a40fa58440a12
tbunitrade <tbunitrade@gmail.com>
2023-09-01 09:49:54 +0300

    Add composer files

---
+++ wp-content/plugins/osony-woocomerce-wishlist/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'osony/osony-woocomerce-wishlist',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '043f3921104915b442a0e2e12eb3ca87dd4552ef',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'osony/osony-woocomerce-wishlist' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '043f3921104915b442a0e2e12eb3ca87dd4552ef',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
